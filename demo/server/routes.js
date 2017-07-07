2c0922df63943df788a53a4c29effc6367ad451e
Jerry Shanahan <jerry.shanahan@gmail.com>
2017-07-06 22:36:11 -0400

    cleanup demo folder

--- demo/server/routes.js
+++ demo/server/routes.js
@@ -3,8 +3,7 @@ const express = require('express');
 const todo = require('./controllers/todoController.js');
 
 module.exports = (app) => {
-  app.get('/', (req, res) => { //put main page where the app lives. route is protected by isLoggedIn
-    //User must be logged in for this to hit
+  app.get('/', (req, res) => {
     app.use(express.static(path.join(__dirname, '/../client')));
     res.sendFile(path.join(__dirname, '/../client/index.html'))
   });
@@ -14,6 +13,6 @@ module.exports = (app) => {
   });
 
   app.post('/api/removeTodo', (req, res) => {
-    todo.removeTodo(req.body.todo, 'default', res); // req.user is what is passed to done in the deserialize function in config.js
+    todo.removeTodo(req.body.todo, 'default', res);
   });
 };
--- demo/server/server.js
+++ demo/server/server.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 require('./routes.js')(app);
+
 let port = 4000;
 app.listen(port,(err) => {
   console.log("Listening on port " + port);
