c93a6d5c79581f2ec97cf1d5165d0efabb6898a9
Jerry Shanahan <jerry.shanahan@gmail.com>
2017-07-08 05:35:52 -0400

    cleanup

--- demo/client/containers/input.js
+++ demo/client/containers/input.js
@@ -7,12 +7,14 @@ class Input extends Component {
   constructor(props){
     super(props);
   }
-  
+
   render(){
     return(
       <div>
+      <form>
         <input id='todoInput' type='text' placeholder='Add To List'/>
-        <button onClick={()=>{ this.props.addTodo(document.getElementById('todoInput').value); document.getElementById('todoInput').value = ''} }>Add Todo</button>
+        <button type="submit" onClick={()=>{ this.props.addTodo(document.getElementById('todoInput').value); document.getElementById('todoInput').value = ''} }>Add Todo</button>
+      </form>
       </div>
     )
   }
